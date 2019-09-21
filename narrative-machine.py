52e8d2f6deba79a91c758f5d0a3565b954989028
Daniel M. Lofaro <dan@danlofaro.com>
2019-09-21 15:51:18 -0400

    added basic sintax

---
+++ narrative-machine.py
@@ -0,0 +1,15 @@
+def set(mot, val):
+# Set motor and angle value for joint
+# Ready to be sent (but not sent)
+
+def put():
+# put all set values on to the robot
+
+def get():
+# get current angle values of robot
+
+def init():
+# initilize the dyn package
+
+def close():
+# stops the dyn packs

d7ea47aaa7f187399cbdede386d15daa43c34970
Neil T. Dantam <ndantam@mines.edu>
2019-12-17 07:18:19 -0700

    Initial ROS play_note

--- midi_inport.py
+++ midi_inport.py
@@ -25,16 +25,34 @@ for i in range(0,8):
     append_num(i)
 
 import mido
+import rospy
+from std_msgs.msg import String
 
 # print(dir(mido.messages))
 # print mido.get_input_names()
 
-with mido.open_input(PORT) as inport:
-    print inport
-    for msg in inport:
-        print '--'
-        if msg.type == 'note_on':
-            print msg
-            print "ON"
-            print(msg.note)
-            print(NOTES[msg.note])
+def msg_str(msg):
+    if msg.type == 'note_on':
+        return NOTES[msg.note]
+    else:
+        return None;
+
+def talker():
+    pub = rospy.Publisher('play_note', String, queue_size=10)
+    rospy.init_node('midi_inport', anonymous=True)
+    #rate = rospy.Rate(10) # 10hz
+    with mido.open_input(PORT) as inport:
+        #print inport
+        for msg in inport:
+            if rospy.is_shutdown(): # TODO: might hang till a note is pressed
+                break
+            s = msg_str(msg)
+            if s is not None:
+                print s
+                pub.publish(s)
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass

4ba9db5c763bfd1d9ad79ab38e612d0147db166b
matthew <matthew@mschack.schack.com>
2019-12-18 08:40:28 -0700

    inverse kinematic solver

---
+++ ik/fk.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+# File: t4.1-fk.py
+# =======================
+#
+# Compute Forward Kinematics
+
+from amino import SceneWin, SceneGraph, SceneFK
+from math import pi, cos
+from time import sleep
+import os
+import sys
+
+# Scene Parameters
+# Change scene_plugin based on your directory structure
+scene_plugin = sys.argv[1]
+scene_name = sys.argv[2]
+
+# Create an (empty) scene graph
+sg = SceneGraph()
+
+# Load the scene plugin
+sg.load(scene_plugin, scene_name)
+
+# Initialize the scene graph
+sg.init()
+
+# Create a window, pass the scenegraph, and start
+win = SceneWin(scenegraph=sg, start=True, background=True)
+
+# Create the FK context
+fk = SceneFK(sg)
+
+# Do a simple wave
+dt = 1.0 / 60
+t = 0
+config_dict = {
+    "j_wrist_l": pi/2,
+    "j_gripper_l": pi/2,
+    "j_shoulder_l": pi/2,
+}
+while win.is_runnining():
+    # update forward kinematics
+    fk.config = config_dict
+
+    # update window
+    win.fk = fk
+
+    # absolute hand pose is changing
+    TF_g_h = fk["right_mallot"]
+    print("g->mallot: %s" % TF_g_h)
+
+    # sleep till next cycle
+    sleep(dt)
+    t += dt
---
+++ ik/ik.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+# File: t4.3-ik.py
+# =======================
+#
+# Compute Inverse Kinematics
+#
+# Slide the end-effector forward and backward in the X-direction
+
+#from amino import SceneWin, SceneGraph, SceneIK, QuatTrans, YAngle
+from math import pi
+from time import sleep
+import os
+import sys
+
+import rospy
+from std_msgs.msg import Float32MultiArray
+
+# Scene Parameters
+# Change scene_plugin based on your directory structure
+scene_plugin = sys.argv[1]
+scene_name = sys.argv[2]
+scene_ee = sys.argv[3]
+
+pub = rospy.Publisher('musicSender', Float32MultiArray, queue_size=10)
+rospy.init_node('robots', anonymous=True)
+
+# Create an (empty) scene graph
+sg = SceneGraph()
+
+# Load the scene plugin
+sg.load(scene_plugin, scene_name)
+
+# Initialize the scene graph
+sg.init()
+
+# Create the sub-scenegraph from root to "hand"
+ssg = sg[:scene_ee]
+
+# Initialize and Start Window
+win = SceneWin(scenegraph=sg, start=True, background=True)
+
+# Create the inverse kinematics context
+ik = SceneIK(ssg)
+
+# IK Parameters
+ik.set_seed_center()
+ik.restart_time = 100e-3
+
+# Set the reference transform
+ik.ref_tf = (-0.199078, 0.678504, 0.199079, -0.678504), (-0.000000, -0.302836, 0.094977)
+
+dt = .1
+period = 3
+while win.is_runnining():
+    # Solve IK
+    q_sub = ik.solve()
+
+    # Display in Window
+    if q_sub:  # check for valid solution
+        print(q_sub)
+        win.config = ssg.scatter_config(q_sub)
+        pub.publish(data=q_sub)
+    else:
+        print("No IK Solution")
+
+    # reseed for next run
+    ik.set_seed_rand()
+
+    # Sleep a bit
+    t = 0
+    while win.is_runnining() and t < period:
+        sleep(dt)
+        t += dt

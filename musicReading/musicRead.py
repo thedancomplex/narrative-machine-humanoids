3597a651662d06b260abc7140c2d27629e8015ac
Matthew Schack <matthewschack5@gmail.com>
2019-12-18 12:26:15 -0700

    Update how tempo and sleep are caluated

--- musicReading/musicRead.py
+++ musicReading/musicRead.py
@@ -3,13 +3,12 @@ import sys
 from std_msgs.msg import String
 
 import mido
-from mido import MidiFile, second2tick, tick2second
+from mido import MidiFile, second2tick, tick2second, bpm2tempo
 
 from time import sleep
 
 MIDI_TICKS_PER_BEAT = -1
 MIDI_TEMPO = 500000 #default tempo
-TRUE_TEMPO = 250000 # microseconds per beat
 
 NOTES = []
 
@@ -31,50 +30,48 @@ append_num(0)
 for i in range(0,8):
     append_num(i)
 
-def parse_note(msg, pub, sleep_acc):
+def parse_note(msg, pub):
     print(msg)
     print("note value: {}".format(NOTES[msg['note']]))
     pub.publish(NOTES[msg['note']])
 
-    # sleep time is in seconds, but we might have a different tempo
-    sleep_time = sleep_acc*TRUE_TEMPO/MIDI_TEMPO
-    
-    print("sleeping for: {}".format(sleep_time))
-    sleep(sleep_time)
+
 
 def parse_tempo(msg):
     print("setting tempo to: {}".format(msg['tempo']))
 
     MIDI_TEMPO = msg['tempo']
-    
 
-def sendSong(song):
+
+def sendSong(song, tempo):
     f = MidiFile(song)
     MIDI_TICKS_PER_BEAT = f.ticks_per_beat
     pub = rospy.Publisher('play_note', String, queue_size=10)
     rospy.init_node('robots', anonymous=True)
 
-    sleep_acc = 0
     for msg in f:
         dict_msg = msg.dict()
         print(dict_msg)
         if 'type' not in dict_msg:
             continue
 
-        sleep_acc += dict_msg['time']
         if dict_msg['type'] == 'note_on':
-            parse_note(dict_msg, pub, sleep_acc)
-            sleep_acc = 0
+            parse_note(dict_msg, pub)
         elif dict_msg['type'] == 'parse_tempo':
             parse_temp(dict_msg)
+
+        # sleep time is in seconds, but we might have a different tempo
+        sleep_time = dict_msg['time']*tempo/MIDI_TEMPO
+
+        print("sleeping for: {}".format(sleep_time))
+        sleep(sleep_time)
 
 
 if __name__ == '__main__':
     f = sys.argv[1]
     pbm = sys.argv[2]
-    TRUE_TEMPO = float(pbm)/60*1000000
+    tempo = bpm2tempo(float(pbm))
     try:
-        sendSong(f)
+        sendSong(f, tempo)
     except rospy.ROSInterruptException:
         pass
-		   

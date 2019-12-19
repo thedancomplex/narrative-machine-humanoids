8b37cd0530cf15fb6ec8750382c39ba101160a89
Matthew Schack <matthewschack5@gmail.com>
2019-12-19 09:24:53 -0700

    simplified code and added a legitament argument parser

--- musicReading/musicRead.py
+++ musicReading/musicRead.py
@@ -1,16 +1,49 @@
 import rospy
-import sys
 from std_msgs.msg import String
+
+import sys
+import argparse
+from time import sleep
 
 import mido
 from mido import MidiFile, second2tick, tick2second, bpm2tempo
 
-from time import sleep
+
 
 MIDI_TICKS_PER_BEAT = -1
 MIDI_TEMPO = 500000 #default tempo
 
 NOTES = []
+
+def parse_args(args):
+    x = argparse.ArgumentParser(
+        "Sends a song note by note over the play_note ROS topic.\nTiming is done as the note should be played in the song.\n")
+
+    x.add_argument(
+        "song",
+        nargs=1,
+        help="The path to a midi file to be played")
+    x.add_argument(
+        "bpm",
+        nargs='?',
+        type=float,
+        help="A custom tempo (in bpm) to set")
+    x.add_argument(
+        "-v",
+        "--verbose",
+        dest="loglevel",
+        help="set loglevel to INFO",
+        action="store_const",
+        const=rospy.INFO)
+    x.add_argument(
+        "-vv",
+        "--very-verbose",
+        dest="loglevel",
+        help="set loglevel to DEBUG",
+        action="store_const",
+        const=rospy.DEBUG)
+    return x.parse_args(args)
+
 
 def append_num(i):
     NOTES.append("C%d" % i)
@@ -31,19 +64,20 @@ for i in range(0,8):
     append_num(i)
 
 def parse_note(msg, pub):
-    print("note value: {}".format(NOTES[msg['note']]))
+    rospy.loginfo("note value: {}".format(NOTES[msg['note']]))
     pub.publish(NOTES[msg['note']])
 
 
 
 def parse_tempo(msg):
-    print("setting tempo to: {}".format(msg['tempo']))
+    rospy.loginfo("setting tempo to: {}".format(msg['tempo']))
 
     MIDI_TEMPO = msg['tempo']
 
 def process_message(msg, pub):
     dict_msg = msg.dict()
-    print(dict_msg)
+
+    rospy.logdebug("{}".format(dict_msg))
     if 'time' not in dict_msg:
         return 0
 
@@ -55,10 +89,10 @@ def process_message(msg, pub):
 
 
 def sendSongTempo(song, tempo):
+    rospy.loginfo("Playing {} with custom tempo: {}".format(song, tempo))
     f = MidiFile(song)
     MIDI_TICKS_PER_BEAT = f.ticks_per_beat
-    pub = rospy.Publisher('play_note', String, queue_size=10)
-    rospy.init_node('robots', anonymous=True)
+
 
     for msg in f.play():
         dict_msg = msg.dict()
@@ -66,13 +100,12 @@ def sendSongTempo(song, tempo):
             # sleep time is in seconds, but we might have a different tempo
             sleep_time = dict_msg['time']*tempo/MIDI_TEMPO
 
-            print("sleeping for: {}".format(sleep_time))
+            rospy.logdebug("sleeping for {}.".format(sleep_time))
             sleep(sleep_time)
 
-def sendSong(song):
+def sendSong(song, pub):
+    rospy.loginfo("Playing {} with their tempo".format(song))
     f = MidiFile(song)
-    pub = rospy.Publisher('play_note', String, queue_size=10)
-    rospy.init_node('robots', anonymous=True)
 
     for msg in f.play():
         process_message(msg, pub)
@@ -80,13 +113,16 @@ def sendSong(song):
 
 
 if __name__ == '__main__':
-    f = sys.argv[1]
-    pbm = sys.argv[2]
-    tempo = bpm2tempo(float(pbm))
+    a = parse_args(sys.argv[1:])
+    f = a.song[0]
+    pub = rospy.Publisher('play_note', String, queue_size=10)
+
+    rospy.init_node('robots', anonymous=True, log_level=a.loglevel)
+
     try:
-        if (len(sys.argv) > 3):
-            sendSong(f)
+        if a.bpm is not None:
+            sendSongTempo(f, bpm2tempo(a.pbm), pub)
         else:
-            sendSongTempo(f, tempo)
+            sendSong(f, pub)
     except rospy.ROSInterruptException:
         pass
